664f5ab554029a1e964cb48e8ce767911411bcbb
Subrata-cisco <saha.subrata@gmail.com>
2014-05-09 16:41:19 +0530

    lamda nd stream eamples for java 8

---
+++ com/java8/date/NewDateAPIExample.java
@@ -0,0 +1,30 @@
+package com.java8.date;
+
+/**
+ * The java.time.zone package provides support for time zones, their rules and the resulting gaps and overlaps 
+ * in the local time-line typically caused by Daylight Saving Time. There is also the java.time.format package 
+ * for printing and parsing date-time objects, although in most cases, the date and time classes' toString() 
+ * and parse() methods should be sufficient. The java.time.temporal package provides access to date and time 
+ * using fields and units, additional value type classes for the most important sub-parts of a date, and base 
+ * support for calendar systems other than the default ISO. This package provides additional functionality for 
+ * more advanced use cases.
+ * 
+ * @author Subrata Saha.
+ *
+ */
+public interface NewDateAPIExample {
+	public static void main(String[] args) {
+		// TODO - Lets see how the following API's work.
+		//Instant time
+		//LocalDate
+		//LocalTime
+		//LocalDateTime
+		//ZonedDateTime
+		//Clock
+		//Duration
+		//Period
+		//ZoneId
+		//ZoneOffset
+		
+	}
+}
--- com/java8/streams/ArrayListWithStreams2.java
+++ com/java8/streams/ArrayListWithStreams2.java
@@ -1,9 +1,13 @@
 package com.java8.streams;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class ArrayListWithStreams2 {
 	public static void main(String[] args) {
@@ -35,5 +39,7 @@ public class ArrayListWithStreams2 {
 		List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
 		IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
 		System.out.println(" stats ::"+stats);
+		
+		
 	}
 }
